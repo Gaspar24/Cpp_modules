890abb26159d9dc4fdf768f7eb26217097e1848a
Marian Sacaliuc <msacaliu@c2r11s9.42wolfsburg.de>
2025-02-07 17:18:13 +0100

    ex01 done

--- CPP06/ex01/main.cpp
+++ CPP06/ex01/main.cpp
@@ -1,6 +1,33 @@
-#include "Serialization.hpp"
+#include "Serializer.hpp"
+#include "Data.hpp"
+
+
 
 int main(void){
 
+
+	Data data;
+	data.grade = 1;
+	data.name = "test";
+
+	std::cout << "name: " + data.name << " has the grade: " << data.grade << std::endl;
+	std::cout << "-----------------------------\n";
+
+	std::cout << "Original Data pointer: " << &data << std::endl;
+
+	std::cout << "-----------------------------\n";
+
+	uintptr_t raw = Serializer::serialize(&data);
+	std::cout << "Serialized: " << raw << std::endl;
+	std::cout << "-----------------------------\n";
+	Data *ptr = Serializer::deserialize(raw);
+
+	std::cout << "Deserialized Data pointer: " << ptr << std::endl;
+	std::cout << "-----------------------------\n";
+
+	if (ptr == &data)
+		std::cout << "success: pointers match!" << std::endl;
+	else
+		std::cout << "error: pointeres don t match!" << std::endl;
 	return 0;
 }
